a966d88627f8da2f9d7ac02a86f52b2c7e969d08
EldenBear <dontet1995@gmail.com>
2023-05-20 10:30:48 -0400

    Created Comment model

---
+++ models/Comment.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model{};
+
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contents: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: "User",
+            key: "userName",
+        }
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "User",
+            key: "id",
+        }
+    },
+    date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+
+    postId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "Post",
+            key: "id",
+        }
+    },
+
+      },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
